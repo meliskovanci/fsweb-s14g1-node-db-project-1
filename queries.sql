0e9bd854460f975be0f0535d77f47370b08e3a7f
Melis Kovancı <meliskovanci@hotmail.com>
2023-04-24 12:24:47 +0300

    queries.sql yapıldı

--- queries.sql
+++ queries.sql
@@ -1,16 +1,22 @@
 -- Veritabanı Sorguları
 
 -- Posta kodu 1010 olan tüm müşterileri bulun
+   SELECT * FROM Customers WHERE PostalCode = 1010;
 
 -- id'si 11 olan tedarikçinin telefon numarasını bulun
+SELECT Phone FROM Suppliers WHERE SupplierID = 11;
 
 -- Verilen ilk 10 siparişi, sipariş tarihine göre azalan şekilde listeleyin
+SELECT * FROM Orders Order By orderdate desc limit 10;
 
 -- Londra, Madrid veya Brezilya'da yaşayan tüm müşterileri bulun
+SELECT * FROM Customers WHERE City='London' OR City='Madrid' OR Country='Brazil';
 
 -- "The Shire" için bir müşteri kaydı ekleyin, ilgili kişi adı "Bilbo Baggins", adres - "Bag End" içinde "1 Hobbit-Hole", posta kodu "111" ve ülke "Middle Earth"
+INSERT into Customers (CustomerName, ContactName, Address, City, PostalCode, Country) values ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth');
 
 -- Posta kodu "11122" olarak değişecek şekilde Bilbo Baggins kaydını güncelleyin
+UPDATE Customers set PostalCode = '11122' WHERE ContactName = 'Bilbo Baggins';
 
 -- (Zorlayıcı Görev) Müşteriler tablosunda kaç farklı şehrin saklandığını keşfetmek için bir sorgu bulun. Tekrarlar çift sayılmamalıdır
 
